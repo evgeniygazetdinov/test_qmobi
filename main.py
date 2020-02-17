9f789e3f07df8e3d83fae8237ab36265e66aac6a
evgeniy gazetdinov <zplacesound@gmail.com>
2020-02-17 08:06:10 +0300

    finish base part.time for test and docker (#1)

--- main.py
+++ main.py
@@ -1,17 +1,69 @@
-import http.server
-import socketserver
 
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import json
+import cgi
+from urlparse import urlparse
+import urllib2
+
+
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def broke_url(self):
+        url = (self.path).split('/')
+        return url
+
+    def get_url_parameters(self):
+        query = urlparse(self.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
+        return query_components
+
+    def check_usd(self):
+        html = 63
+        url= ("https://api.exchangeratesapi.io/latest?base=USD")
+        try:
+            response = urllib2.urlopen(url)
+            html = json.load(response)
+        except:
+            pass
+            return html
+        return html['rates']['RUB']
+
+
+
     def do_GET(self):
-        if self.path == '/':
-            self.path = 'mywebpage.html'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+        self._set_headers()
+        your_request = self.broke_url()
+        if your_request[1] == 'convert':
+            parameters = self.get_url_parameters()
+            try:
+                usd = int(parameters["Amount"])
+            except ValueError:
+                return self.wfile.write(json.dumps({'wrong': 'amount', 'received': 'ok'}))
+            if type(usd) == int:
+                rub_couse = self.check_usd()
+                res = usd*rub_couse
+                self.wfile.write(json.dumps({'RUB': res, 'received': 'ok'}))
+        else:
+            self.wfile.write(json.dumps({'wrong': 'query', 'received': 'ok'}))
 
-# Create an object of the above class
-handler_object = MyHttpRequestHandler
 
-PORT = 8000
-my_server = socketserver.TCPServer(("", PORT), handler_object)
 
-# Star the server
-my_server.serve_forever()
+def run(server_class=HTTPServer, handler_class=Server, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+
+    print('Starting httpd on port {}...'.format(port))
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
